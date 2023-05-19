0d2df9e31c05069f15e99601917e75018383ebbe
Rodney <101829484+SranangMang1@users.noreply.github.com>
2023-05-19 13:42:24 -0400

    class for record

--- Objects/RecordDTO.cs
+++ Objects/RecordDTO.cs
@@ -2,8 +2,5 @@
 
 public class RecordDTO
 {
-    public int Id { get; set; }
-    public int Temprature { get; set; }
-    public int LocationId { get; set; }
-    public int WeatherDescriptionId { get; set; }
+   
 }
